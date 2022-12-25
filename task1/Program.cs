02f47c4abc90eed8e311f353d0f49d84390f60bc
Ilya <osetia9595@gmail.com>
2022-12-25 23:49:52 +0300

    tsk1

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("input 5-digit number");
+int x = int.Parse(Console.ReadLine());
+int temp = x;
+int r = 0;
+int y = 0;
+
+while (x > 0)
+{
+r = x % 10;
+y = (y * 10) + r;
+x = x / 10;
+}
+
+
+if (temp == y) 
+{
+Console.WriteLine("it's a Palindrome");
+}
+
+else
+{
+    Console.WriteLine("it's not a palindrome");
+}
