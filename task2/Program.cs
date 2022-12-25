a22d227f7f7a490631b17e321f7b988847437bee
Ilya <osetia9595@gmail.com>
2022-12-26 00:12:35 +0300

    tsk2

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("input x-coordinate A");
+int xA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("input y-coordinate A");
+int yA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("input z-coordinate A");
+int zA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("input x-coordinate B");
+int xB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("input y-coordinate B");
+int yB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("input z-coordinate B");
+int zB = int.Parse(Console.ReadLine());
+
+double distance = 0;
+
+//√ (x2-x1)^2)+ (y2-y1)^2+ (z2-z1)^2
+
+distance = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2) + Math.Pow(zB - zA, 2));
+Console.WriteLine(Math.Round(distance, 3));
